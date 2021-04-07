88eb19300f3ce1f3d7342e5b2388733a8beb947d
Serj-L <svlepniakov@gmail.com>
2021-04-07 17:38:53 +0300

    getDistanceBetweenPoints

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -54,6 +54,11 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
+    /* let sum = 0;
+        for (let i = 0; i < arguments.length; i++) {
+        sum += arguments[i];
+        }
+    return sum / arguments.length; */
     throw new Error('Not implemented');
 }
 
@@ -73,7 +78,8 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    return Math.sqrt(((x2 - x1) ** 2) + ((y2 - y1) ** 2));
+
 }
 
 /**
