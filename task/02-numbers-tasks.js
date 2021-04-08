4e75164c41fa61c10a67edf0bb861a4abd9c77c4
Serj-L <svlepniakov@gmail.com>
2021-04-08 15:54:59 +0300

    getAngleBetweenVectors

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -116,7 +116,11 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    const lenghtVectorA = Math.sqrt(x1 ** 2 + y1 ** 2);
+    const lenghtVectorB = Math.sqrt(x2 ** 2 + y2 ** 2);
+    const scalarVectors = x1 * x2 + y1 * y2;
+    const cosVectors = scalarVectors / (lenghtVectorA * lenghtVectorB);
+    return Math.acos(cosVectors);
 }
 
 /**
