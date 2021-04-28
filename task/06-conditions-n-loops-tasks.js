469a669d2e3ee8a3f7a1097771caaf39b85a81d4
Serj-L <svlepniakov@gmail.com>
2021-04-28 16:15:07 +0300

    isTriangle

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -90,6 +90,7 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,10,10 =>  true
  */
 function isTriangle(a,b,c) {
+    return a + b > c && a + c > b && b + c > a ? true : false;
     throw new Error('Not implemented');
 }
 
