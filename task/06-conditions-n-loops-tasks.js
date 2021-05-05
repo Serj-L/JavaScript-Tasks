6182f77b16c11919f76ecb0c524c5c83aafc4ba6
Serj-L <svlepniakov@gmail.com>
2021-05-05 12:00:55 +0300

    getCommonDirectoryPath

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -469,7 +469,20 @@ return num.toString(n);
  *   ['/web/favicon.ico', '/web-scripts/dump', '/webalizer/logs'] => '/'
  */
 function getCommonDirectoryPath(pathes) {
-    throw new Error('Not implemented');
+    let destructingPathes = pathes.join('').split('/');
+    let allRepeatingValues = destructingPathes.filter((el, ind, arr) => ind !== arr.indexOf(el) || ind !== arr.lastIndexOf(el));
+    let repeatingValuesCount = allRepeatingValues.reduce((acc, el) => (typeof acc[el] !== 'undefined') ? {...acc, [el]: acc[el] + 1} : {...acc, [el]: 1} , {});
+    let repeatingValues = [];
+        for (let key in repeatingValuesCount) {
+            if (repeatingValuesCount[key] === pathes.length) {
+                repeatingValues.push(key)
+            }
+        }
+    let uniqValues = repeatingValues.reduce((acc, el) => acc.includes(el) ? acc : [...acc, el], []);
+    let commonPath = uniqValues.map((el, ind) => ind === uniqValues.length - 1 ? el = `/${el}/` : el = `/${el}`).join('').split();
+    let checkFirstChar = pathes.filter(el => el.charAt(0) === '/').length === pathes.length;
+
+    return commonPath[0] === '' && checkFirstChar === true ? '/' : commonPath.join('');
 }
 
 
