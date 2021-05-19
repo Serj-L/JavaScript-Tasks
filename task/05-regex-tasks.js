635d940ccfc55397c8e024c13c61bed310104c48
Serj-L <svlepniakov@gmail.com>
2021-05-19 12:40:20 +0300

    getPasswordValidator

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -116,7 +116,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+   return new RegExp('^(?=.*[0-9])(?=.*[A-Z])(?=.*[a-z])([0-9A-Za-z]{'+minLength+',})$')
 }
 
 
